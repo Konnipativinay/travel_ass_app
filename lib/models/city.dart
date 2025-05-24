da1d747b72107d519505be4d3fb360eff54d7b71
Konnipativinay <skonnipati72@jnn.edu.in>
2025-05-24 15:41:47 +0530

    Add files via upload

---
+++ lib/models/city.dart
@@ -0,0 +1,17 @@
+class City {
+  final String name;
+  final String state;
+  final String bestTimeToVisit;
+  final String imageUrl;
+  final List<String> popularPlaces;
+  final List<String> popularRestaurants;
+
+  City({
+    required this.name,
+    required this.state,
+    required this.bestTimeToVisit,
+    required this.imageUrl,
+    required this.popularPlaces,
+    required this.popularRestaurants,
+  });
+}
